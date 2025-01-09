a01b7a370cfca5d770b34d9f93e5c85868d07ff2
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-01-08 22:33:04 -0700

    write middlewares for checkAccountPayload. name and budget required, length requirements set

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,9 +1,31 @@
 const Account = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-  console.log('checkAccountPayload middleware')
-  next()
-}
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if(name === undefined || budget === undefined) {
+    error.message = 'name and budget are required'
+    next(error)
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  } else if (typeof budget !== 'number') {
+    error.message = 'budget of account must be a number'
+    next(error)
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+  }
+
+  if (error.message) {
+    next(error)
+    } else {
+        next()
+      }
+    }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   console.log('checkAccountPayload middleware')
